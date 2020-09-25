3d6972568011041e8192fab8c53415a527afc34a
Matthew Hodgetts <mpolar999@gmail.com>
2020-09-25 05:17:43 +0100

    Feature row done

---
+++ src/components/common/FeatureRow/FeatureRow.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+import "./FeatureRow.scss";
+
+import { Link } from "gatsby";
+import { BsArrowRight } from "react-icons/bs";
+
+const FeatureRow = ({
+  title,
+  text,
+  linkText,
+  linkUrl,
+  imgUrl,
+  white,
+  alternate,
+  special,
+}) => {
+  let color;
+  if (white) {
+    color = "white";
+  } else {
+    color = "black";
+  }
+
+  let direction;
+
+  if (alternate) {
+    direction = "flex-row-reverse";
+  } else {
+    direction = "";
+  }
+
+  let highlight;
+
+  if (special) {
+    highlight = "feature-row__content--special";
+  } else {
+    highlight = "";
+  }
+
+  return (
+    <section className={`feature-row feature-row--${color}`}>
+      <div className="container-fluid">
+        <div className={`row ${direction}`}>
+          <div className="col-md-6 col-xl-5">
+            <div className={`feature-row__content ${highlight} `}>
+              <h2 className="feature-row__title">{title}</h2>
+              <p className="feature-row__text my-3">{text}</p>
+              <Link to={linkUrl}>
+                {linkText} <BsArrowRight />{" "}
+              </Link>
+            </div>
+          </div>
+          <div
+            className="col-md-6 col-xl-7 feature-row__img"
+            style={{ backgroundImage: `url('${imgUrl}')` }}
+          ></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+FeatureRow.defaultProps = {
+  title: "Title here",
+  text: "Lorem ipsum dolor sit amet....",
+  linkText: "Text for link",
+  linkUrl: "/",
+  imgUrl: "https://via.placeholder.com/830x650",
+  white: false,
+  alternate: false,
+  special: false,
+};
+
+export default FeatureRow;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,37 @@ import React from "react";
 import Layout from "@common/Layout/Layout";
 import SEO from "@common/Seo/Seo";
 
+import FeatureRow from "@common/FeatureRow/FeatureRow";
+
+import img1 from "../images/home/create-and-share.jpg";
+import img2 from "../images/home/beautiful-stories.jpg";
+import img3 from "../images/home/designed-for-everyone.jpg";
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <FeatureRow
+      title="Create and share your photo Stories."
+      text="Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others."
+      imgUrl={img1}
+      linkText="Get an invite"
+      special
+    />
+    <FeatureRow
+      title="Beautiful stories
+      every time"
+      text="We provide design templates to ensure your stories look terrific. Easily add photos, text, embed maps and media from other networks. Then share your story with everyone."
+      alternate
+      white
+      imgUrl={img2}
+      linkText="View the stories"
+    />
+    <FeatureRow
+      title="Create and share your photo Stories."
+      text="Photosnap can help you create stories that resonate with your audience. Our tool is designed for photographers of all levels, brands, businesses you name it."
+      imgUrl={img3}
+      linkText="View the stories"
+    />
   </Layout>
 );
 
